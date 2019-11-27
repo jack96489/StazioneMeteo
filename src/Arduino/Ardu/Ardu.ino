aff018ad6ee9333a44efc28dbf6ed476f8edcb0c
jack96489 <giacomo.orsenigo@gmail.com>
2019-11-27 17:45:14 +0100

    Sketch Arduino

---
+++ src/Arduino/Ardu/Ardu.ino
@@ -0,0 +1,32 @@
+#define LED_PIN 2
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (Serial.available()) {
+    byte read = Serial.read();
+    Serial.print(read);
+
+    switch (read) {
+      case 2:
+        Serial.print("ACCENDO");
+        digitalWrite(LED_PIN, HIGH);
+        Serial.write("OK");    //confermo
+        break;
+      case 3:
+        Serial.print("SPENGO");
+        digitalWrite(LED_PIN, LOW);
+        Serial.write("OK");    //confermo
+        break;
+      default:
+        Serial.write("Comando non conoscuito");
+    }
+  }
+  delay(10);
+}
