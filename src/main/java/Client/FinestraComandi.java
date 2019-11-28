9f3e6abea5b1fe6dbfe7bbcbfe3394679f1afb39
Turati-Marco <Marco@DESKTOP-6O9OGJE>
2019-11-28 22:13:44 +0100

    POTENZIOMETRO

--- src/main/java/Client/FinestraComandi.java
+++ src/main/java/Client/FinestraComandi.java
@@ -35,7 +35,7 @@ public class FinestraComandi extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jTextArea1.setEditable(false);
         jTextArea1.setColumns(20);
